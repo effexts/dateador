73d05e90f5c19ae09aa867fc4018b5183a1469de
Hacking chile <info@hacking.cl>
2017-03-13 23:48:15 -0300

    Corrección de error de conexión.
    Se soluciona el error de conexión al sitio rutificador.com

--- dateador.py
+++ dateador.py
@@ -12,7 +12,7 @@ import sys
 
 # definimos la hora 
 fecha = datetime.now()
-hora = '%s/%s/%s a las %s:%s:%s' % (fecha.day, fecha.month, fecha.year, fecha.hour, fecha.minute, fecha.second)
+hora  = '%s/%s/%s a las %s:%s:%s' % (fecha.day, fecha.month, fecha.year, fecha.hour, fecha.minute, fecha.second)
 
 # Mensaje de inicio
 print colored("""
@@ -22,7 +22,7 @@ print colored("""
                     ║║║║╔╗║║║║═╣╔╗║╔╗║╔╗║╔╝
                    ╔╝╚╝║╔╗║╚╣║═╣╔╗║╚╝║╚╝║║
                    ╚═══╩╝╚╩═╩══╩╝╚╩══╩══╩╝
-                      v1.0 by @unkndown
+                      v1.1 by @unkndown
 ""","blue", attrs=['bold'])
 # Opciones
 print colored(" Obtén los datos de una persona con el rut o nombre de ella: \n\n -Ejemplo con rut: 5519653-2 \n -Ejemplo con nombre: Pedro Aguilar Toloza\n -Si tienes solo una parte del nombre, usa la opción stalker\n", "magenta", attrs=['bold'])
@@ -36,14 +36,13 @@ try:
 
     nombre     = raw_input(" Ingresa un rut o nombre: ")
     print colored(" \n [+] Buscando datos \n","green",attrs =['bold'])
-    link       = "http://chile.rutificador.com/get_generic_ajax/"
+    link       = "https://chile.rutificador.com/get_generic_ajax/"
     host       = "chile.rutificador.com:80"
-    parametros = urllib.urlencode({'csrfmiddlewaretoken':'2','entrada':nombre})
-    headers    = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain","Cookie":"csrftoken=2"}
-    conexion   = httplib.HTTPConnection(host)
-    conexion.request("POST", link, parametros, headers)
-    respuesta  = conexion.getresponse()
-    ver_source = respuesta.read()
+    headers    = {'Origin': 'https://chile.rutificador.com','Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'es-ES,es;q=0.8', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Accept': '*/*', 'Referer': 'https://chile.rutificador.com/', 'X-Requested-With': 'XMLHttpRequest', 'Connection': 'keep-alive', }
+    cookies    = {'_gat': '1', 'csrftoken': 'W9s4ydeFOSbl37Gp3iYnOMDBIGbYz7AW', '_ga': 'GA1.2.62259178.1488421266', }
+    parametros = {'csrfmiddlewaretoken': 'W9s4ydeFOSbl37Gp3iYnOMDBIGbYz7AW', 'entrada': nombre }
+    respuesta  = requests.post('https://chile.rutificador.com/get_generic_ajax/', headers=headers, cookies=cookies, data=parametros)
+    ver_source = respuesta.text
     data       = json.loads(ver_source)
 
     # verificamos si se ha encontrado un dato 
@@ -86,7 +85,6 @@ try:
         # 
         # Obtenemos la información a partir del rut obtenido
         # 
-
         url     = "http://buscardatos.com/cl/personas/padron_cedula_chile.php"
         hosts   = "buscardatos.com:80"
         post    = urllib.urlencode({'cedula': rut})
